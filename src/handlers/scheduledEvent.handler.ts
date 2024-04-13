06883b3531f3da27b300c2ef34e9c6a4c5066225
Samuel Jurado Quintana <samueljurado12@gmail.com>
2024-04-13 11:27:24 +0200

    Basic implementation of ScheduledEvent notification

--- src/handlers/scheduledEvent.handler.ts
+++ src/handlers/scheduledEvent.handler.ts
@@ -1,5 +1,24 @@
-import { GuildScheduledEvent } from "discord.js";
+import { discordRestClient } from "#core/clients/discord-rest.client.js";
+import { serverConfigRepository } from "#dals/server-config/repositories";
+import { GuildScheduledEvent, REST, Routes } from "discord.js";
 
 export const onScheduledEventCreate = async (scheduledEvent: GuildScheduledEvent) => {
+    const { guildId, url } = scheduledEvent
 
+    const { notificationChannelId, notificationRoleId } = await serverConfigRepository?.getServerConfig(guildId);
+    if (!notificationChannelId || !notificationRoleId) return; //TODO Add error handler. Missing configuration
+
+    await discordRestClient.post(
+        Routes.channelMessages(notificationChannelId),
+        {
+            body: {
+                content: `Esta es una prueba de mensaje para ver si el bot funciona, además añado el enlace ${url} y a ver si la mencion funciona <@&${notificationRoleId}>`,
+                allowedMentions: {
+                    parse: ["roles"],
+                    roles: [notificationRoleId]
+                }
+            }
+
+        }
+    )
 }
